a9bde6293083744e6fabacd2afe97ca9cba17200
pofnor <142336027+pofnor@users.noreply.github.com>
2023-08-17 13:16:08 +0330

    regexp

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const menuItem = [  
+  {id:"jsClass"     , href:"regExp.html"                 , text:"RegExp"},
   {id:"jsClass"     , href:"jsClassQuestion.html"        , text:"Question"},
   {id:"jsClass"     , href:"jsClassSession1.html"        , text:"JS Class Session 1"},
   {id:"jsClass"     , href:"jsClassSession2.html"        , text:"JS Class Session 2"},
   {id:"jsClass"     , href:"jsClassSession3.html"        , text:"JS Class Session 3"},
-  {id:"jsClass"     , href:"jsClassSession4.html"        , text:"JS Class Session 4"},
+  {id:"jsClass"     , href:"jsClassSession4.html"        , text:"JS Class Session 4"},  
 ]
 
 function menuFilter(menuItem,id,isHome){  
