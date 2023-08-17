e11a3376dcd34a81246e44b27e5e7f506872eaea
pofnor <142336027+pofnor@users.noreply.github.com>
2023-08-17 13:00:33 +0330

    js session3 and 4 added

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,9 @@
 const menuItem = [  
   {id:"jsClass"     , href:"jsClassQuestion.html"        , text:"Question"},
   {id:"jsClass"     , href:"jsClassSession1.html"        , text:"JS Class Session 1"},
-  {id:"jsClass"     , href:"jsClassSession2.html"        , text:"JS Class Session 2"},  
+  {id:"jsClass"     , href:"jsClassSession2.html"        , text:"JS Class Session 2"},
+  {id:"jsClass"     , href:"jsClassSession3.html"        , text:"JS Class Session 3"},
+  {id:"jsClass"     , href:"jsClassSession4.html"        , text:"JS Class Session 4"},
 ]
 
 function menuFilter(menuItem,id,isHome){  
