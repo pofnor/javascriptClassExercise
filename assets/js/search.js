304204ea4d4ae9e1da40a5e1f779d9324fd758a2
pofnor <142336027+pofnor@users.noreply.github.com>
2023-08-22 22:28:21 +0330

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -197,50 +197,51 @@ function search() {
   }  
 }
 
-function autocomplete(inp, arr) {
+function autocomplete(searchbarElement, indexDBKeyword) {
   /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
-  var currentFocus;  
-  /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {      
-      let a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();      
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");      
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {          
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/          
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";          
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              search(); //run search for fixed the click problem
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();              
-          });
-          a.appendChild(b);
-        }
+  the search field element and an indexDBKeyword(array) of possible autocompleted values:*/
+  let currentFocus;  
+  /*execute a function when someone writes in the search field:*/
+  searchbarElement.addEventListener("input", function(e) {      
+    let searchText = this.value;
+    let autocompleteItems, matchingItem;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();      
+    if (!searchText) { return false;}
+    currentFocus = -1;
+    /*create a Section element that will contain the searchText(values):*/
+    autocompleteItems = document.createElement("section");      
+    autocompleteItems.setAttribute("id", this.id + "autocomplete-list");
+    autocompleteItems.setAttribute("class", "autocomplete-items");
+    /*append the Section element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(autocompleteItems);
+    /*for each item in the indexDBKeyword(array)...*/
+    for (let keyword of indexDBKeyword) {
+      /*check if the item starts with the same letters as the search field value:*/
+      if (keyword.substr(0, searchText.length).toUpperCase() == searchText.toUpperCase()) {          
+        /*create a DIV element for each matching element:*/
+        matchingItem = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        matchingItem.innerHTML = "<strong>" + keyword.substr(0, searchText.length) + "</strong>";
+        matchingItem.innerHTML += keyword.substr(searchText.length);          
+        /*insert a input field that will hold the current array item's value:*/          
+        matchingItem.innerHTML += "<input type='hidden' value='" + keyword + "'>";          
+        /*execute a function when someone clicks on the item value (DIV element):*/
+          matchingItem.addEventListener("click", function(e) {
+          /*insert the value for the autocomplete search field:*/          
+          searchbarElement.value = this.getElementsByTagName("input")[0].value;
+          search(); //run search for fixed the click problem and show the result
+          /*close the list of autocompleted values,
+          (or any other open lists of autocompleted values:*/
+          closeAllLists();              
+        });
+        autocompleteItems.appendChild(matchingItem);
       }
+    }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
+  searchbarElement.addEventListener("keydown", function(e) {
+      let x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
       if (e.keyCode == 40) {
         /*If the arrow DOWN key is pressed,
@@ -273,16 +274,16 @@ function autocomplete(inp, arr) {
   }
   function removeActive(x) {
     /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
+    for (let i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
   function closeAllLists(elmnt) {
     /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/    
+    except the one passed as an argument:*/
     let x = document.getElementsByClassName("autocomplete-items");    
     for (let i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
+      if (elmnt != x[i] && elmnt != searchbarElement) {
       x[i].parentNode.removeChild(x[i]);
     }
   }
