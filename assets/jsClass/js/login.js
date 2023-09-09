330bfc7ef2bee90ebca6a5de4b889639bb7455b2
Ahmad <142336027+pofnor@users.noreply.github.com>
2023-09-09 20:57:25 +0330

    update login

--- assets/jsClass/js/login.js
+++ assets/jsClass/js/login.js
@@ -4,10 +4,42 @@ function login(){
   const result = document.getElementById("result");
   const username = document.getElementById("username");
   const password = document.getElementById("password");
-  result.textContent = "You click on Login button";
+  const signupBtn = document.getElementById("signupBtn");
+  const loginBtn = document.getElementById("loginBtn");
+  const loginHeader = document.getElementById("loginHeader");
+
+  axios.get("http://localhost:3000/account")
+  .then((response) => {
+    // console.log(response.data);
+    // console.log(response.status);
+    // console.log(response.statusText);
+    // console.log(response.headers);
+    // console.log(response.config);
+    for (let index in response.data){
+      if ((response.data[index].username === username.value.trim()) &&
+      (response.data[index].password === password.value)){
+        result.innerHTML = "Welcome <span>" + response.data[index].name + "</span><br>" + "You have successfully logged in";
+        signupBtn.style.display = "none";
+        loginBtn.style.display = "none";
+        username.style.display = "none";
+        password.style.display = "none";
+        loginHeader.style.display = "none";
+        break;
+      } else {
+        result.textContent = "Wrong Username or Password";
+      }
+    }
+  })
+  .catch(error => {
+    result.innerHTML = error.message + "<br>" + error.config.url;
+  })
+
+
+  // result.textContent = "You click on Login button";
 }
 
 function signup(){
+  const loginHeader = document.getElementById("loginHeader");
   const username = document.getElementById("username");
   const password = document.getElementById("password");
   const loginBtn = document.getElementById("loginBtn");
@@ -26,6 +58,7 @@ function signup(){
   let mobile = document.getElementById("mobile");
   loginContainer.insertBefore(nameInput,mobile);
   loginBtn.style.display = "none" ;
+  loginHeader.textContent = "Create New Account";
   signupBtn.textContent = "Sign up";  
   result.textContent = "You click on signup button";
 }
