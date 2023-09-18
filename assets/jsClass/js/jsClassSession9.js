4382ea56df919d399e3149ae343351154e6f0362
pofnor <142336027+pofnor@users.noreply.github.com>
2023-09-18 15:39:26 +0330

    add webcam to jsClassSession9

--- assets/jsClass/js/jsClassSession9.js
+++ assets/jsClass/js/jsClassSession9.js
@@ -60,3 +60,30 @@ function numberSeparately3() {
   // The result will be 987,654,321.00
   numberSep.textContent = numberPart.replace(thousands, ",") + (decimalPart ? "." + decimalPart : "");
 }
+
+// -------------------------- Webcam ---------------------------------------
+function openCam(){
+  let All_mediaDevices=navigator.mediaDevices
+  if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+     console.log("getUserMedia() not supported.");
+     return;
+  }
+  All_mediaDevices.getUserMedia({
+     audio: true,
+     video: true
+  })
+  .then(function(vidStream) {
+     var video = document.getElementById('videoCam');
+     if ("srcObject" in video) {
+        video.srcObject = vidStream;
+     } else {
+        video.src = window.URL.createObjectURL(vidStream);
+     }
+     video.onloadedmetadata = function(e) {
+        video.play();
+     };
+  })
+  .catch(function(e) {
+     console.log(e.name + ": " + e.message);
+  });
+}
