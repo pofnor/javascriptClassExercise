0cb489b7bf038800fd93723d83941504ae497048
pofnor <142336027+pofnor@users.noreply.github.com>
2023-09-19 13:02:16 +0330

    update webcam on jsClassSession9

--- assets/jsClass/js/jsClassSession9.js
+++ assets/jsClass/js/jsClassSession9.js
@@ -63,27 +63,28 @@ function numberSeparately3() {
 
 // -------------------------- Webcam ---------------------------------------
 function openCam(){
-  let All_mediaDevices=navigator.mediaDevices
-  if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
-     console.log("getUserMedia() not supported.");
-     return;
-  }
-  All_mediaDevices.getUserMedia({
-     audio: false,
-     video: true
-  })
-  .then(function(vidStream) {
-     var video = document.getElementById('videoCam');
-     if ("srcObject" in video) {
-        video.srcObject = vidStream;
-     } else {
-        video.src = window.URL.createObjectURL(vidStream);
-     }
-     video.onloadedmetadata = function(e) {
-        video.play();
-     };
-  })
-  .catch(function(e) {
-     console.log(e.name + ": " + e.message);
-  });
+   const prompt = document.getElementById("webcamPrompt");   
+   let All_mediaDevices=navigator.mediaDevices
+   if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+      prompt.textContent = "getUserMedia() not supported.";
+      return;
+   }
+   All_mediaDevices.getUserMedia({
+      audio: false,
+      video: true
+   })
+   .then(function(vidStream) {
+      var video = document.getElementById('videoCam');
+      if ("srcObject" in video) {
+         video.srcObject = vidStream;
+      } else {
+         video.src = window.URL.createObjectURL(vidStream);
+      }
+      video.onloadedmetadata = function(e) {
+         video.play();
+      };
+   })
+   .catch(function(e) {
+      prompt.textContent = e.name + ": " + e.message;
+   });
 }
