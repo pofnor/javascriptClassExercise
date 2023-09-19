46bd26aabbc39220583904d361e158785bbccdb2
pofnor <142336027+pofnor@users.noreply.github.com>
2023-09-19 12:50:09 +0330

    update style of map

--- assets/jsClass/js/map.js
+++ assets/jsClass/js/map.js
@@ -39,16 +39,31 @@ function showMap(){
 }
 
 // --------------- Client Location -----------------------
-function getLocation() {
-  const x = document.getElementById("demo");
+function getLocation() { 
   if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
   } else { 
-    document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser.";
+    document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
   }
 }
     
 function showPosition(position) {
-  document.getElementById("demo").innerHTML="Latitude: " + position.coords.latitude + 
+  document.getElementById("location").innerHTML="Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
+}
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      document.getElementById("location").innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      document.getElementById("location").innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      document.getElementById("location").innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      document.getElementById("location").innerHTML = "An unknown error occurred."
+      break;
+  }
 }
