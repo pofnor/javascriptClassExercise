0c11dbf77a0cc1de75d464cf55a430f67e1af8a2
Ahmad <142336027+pofnor@users.noreply.github.com>
2023-09-19 21:34:25 +0330

    complete map

--- assets/jsClass/js/map.js
+++ assets/jsClass/js/map.js
@@ -2,48 +2,52 @@ let isMarker = false;
 let isCircle = false;
 
 function showMap(){
-  var map = L.map('map').setView([35.714183, 51.365204], 11);
+  const map = L.map('map').setView([35.714183, 51.365204], 11);
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 15,    
     }).addTo(map);
 
-    var marker = L.marker([35.678494, 51.306839]).addTo(map);
+    // const marker = L.marker([35.678494, 51.306839]).addTo(map);
 
-    var circle = L.circle([35.695504, 51.469917], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-    }).addTo(map);
+    // const circle = L.circle([35.695504, 51.469917], {
+    // color: 'red',
+    // fillColor: '#f03',
+    // fillOpacity: 0.5,
+    // radius: 500
+    // }).addTo(map);
 
-    var polygon = L.polygon([
-    [35.769079, 51.345978],
-    [35.769079, 51.38855],
-    [35.74902, 51.384773]
-    ]).addTo(map);
+    // var polygon = L.polygon([
+    // [35.769079, 51.345978],
+    // [35.769079, 51.38855],
+    // [35.74902, 51.384773]
+    // ]).addTo(map);
 
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
-    polygon.bindPopup("I am a polygon.");
+    // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    // circle.bindPopup("I am a circle.");
+    // polygon.bindPopup("I am a polygon.");
 
-    var popup = L.popup();
+    const popup = L.popup();
 
-    // function onMapClick(e) {
-    //   popup
-    //     .setLatLng(e.latlng)
-    //     .setContent("You clicked the map at " + e.latlng.toString())
-    //     .openOn(map);
-    // }
-
-    function onMapClick(e) {
-      document.getElementById("address").textContent = e.latlng.toString();
-      if(isMarker){
-        let m = L.marker([e.latitude,e.longitude]).addTo(map);
+    function onMapClick(e) {           
+      showLoader1(true);
+      axios.get(`https://geocode.maps.co/reverse?lat=${e.latlng.lat}&lon=${e.latlng.lng}`)
+      .then((response) => {
+        showLoader1(false);
+        document.getElementById("address").innerHTML = "Latitude: " + e.latlng.lat + 
+        "<br>Longitude: " + e.latlng.lng +
+        "<br>Address = " + response.data.display_name;
+      })
+      .catch(error=>{
+        showLoader1(false);
+        document.getElementById("address").textContent = "Error=" + error.message ;
+      });
+      if(isMarker){        
+        let m = L.marker([e.latlng.lat,e.latlng.lng]).addTo(map);
         m.bindPopup(e.latlng.toString());        
       }
       if(isCircle){
-        let c = L.circle([e.latitude,e.longitude], {
+        let c = L.circle([e.latlng.lat,e.latlng.lng], {
           color: 'red',
           fillColor: '#f03',
           fillOpacity: 0.5,
@@ -60,13 +64,13 @@ function showMap(){
 
     map.on('click', onMapClick);
 
-    // https://www.geoapify.com/ to reverse geocoding
+    // https://geocode.maps.co/reverse?lat=35.648927&lon=51.366583
 }
 
 // --------------- Client Location -----------------------
 function getLocation() { 
   if (navigator.geolocation) {
-    showLoader(true);
+    showLoader2(true);
     navigator.geolocation.getCurrentPosition(showPosition,showError);
   } else { 
     document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
@@ -74,12 +78,12 @@ function getLocation() {
 }
     
 function showPosition(position) {
-  showLoader(false);
+  showLoader2(false);
   document.getElementById("location").innerHTML="Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
 }
 function showError(error) {
-  showLoader(false);
+  showLoader2(false);
   switch(error.code) {
     case error.PERMISSION_DENIED:
       document.getElementById("location").innerHTML = "User denied the request for Geolocation."
@@ -96,11 +100,14 @@ function showError(error) {
   }
 }
 
-
 // --------------------- Hide and Show Loader ------------------------------------------------------
-function showLoader(isShow){
-  if(isShow) document.getElementById("loader").style.display="block"; // Show Loader
-  else document.getElementById("loader").style.display="none"; // Hide Loader
+function showLoader1(isShow){
+  if(isShow) document.getElementById("loader1").style.display="block"; // Show Loader
+  else document.getElementById("loader1").style.display="none"; // Hide Loader
+}
+function showLoader2(isShow){
+  if(isShow) document.getElementById("loader2").style.display="block"; // Show Loader
+  else document.getElementById("loader2").style.display="none"; // Hide Loader
 }
 
 // --------------------- Map Marker ------------------------------------------------------
